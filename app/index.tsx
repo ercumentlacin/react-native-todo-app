7da5bcb11a458cc6f2a8b5ffd5259a2f5b9645bb
ercumentlacin <forever.ducale@gmail.com>
2024-03-25 16:57:29 +0300

    feat: add storybook

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,26 @@
 import { Redirect } from "expo-router";
 
+// app/Index.tsx
+import React from "react";
+import { View } from "react-native";
+
+const storybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true";
+
 const Index = () => {
 	return <Redirect href={"/(account)/register"} />;
 };
 
-export default Index;
+let EntryPoint = Index;
+
+if (storybookEnabled) {
+	const StorybookUI = require("../.storybook").default;
+	EntryPoint = () => {
+		return (
+			<View style={{ flex: 1 }}>
+				<StorybookUI />
+			</View>
+		);
+	};
+}
+
+export default EntryPoint;
