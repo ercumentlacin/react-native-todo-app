5d9fc6f0065888ef6694413f783167a134894466
ercumentlacin <forever.ducale@gmail.com>
2024-03-25 07:57:11 +0300

    feat: add fontSize, fontWeight, letterSpacing, lineHeight constants

---
+++ constants/letterSpacing.ts
@@ -0,0 +1,13 @@
+const scaling = 16;
+
+export const letterSpacing = {
+	1: 0.0025 * scaling,
+	2: 0 * scaling,
+	3: 0 * scaling,
+	4: -0.0025 * scaling,
+	5: -0.005 * scaling,
+	6: -0.00625 * scaling,
+	7: -0.0075 * scaling,
+	8: -0.01 * scaling,
+	9: -0.025 * scaling,
+};
